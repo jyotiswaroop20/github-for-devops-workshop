e8a5a22939a98938d3155055246679ecd3253f21
jyotiswaroop20 <jyotiswaroop.niit1@gmail.com>
2025-08-08 22:56:28 +0530

    Added test

--- demo.py
+++ demo.py
@@ -2,12 +2,14 @@
 This is a demo program that asks the user for their name and greets them.
 """
 
+
 def main():
     """
     This function asks the user for their name and prints a greeting.
     """
     name = input("What is your name? ")
     print(f"Namaste, {name}!")
+
 
 if __name__ == "__main__":
     main()
