ba392c368a14a364cf345460417471867f34b50e
jyotiswaroop20 <jyotiswaroop.niit1@gmail.com>
2025-08-16 15:54:09 +0530

    Add file

--- testing.py
+++ testing.py
@@ -1,2 +1,13 @@
-print("This is the testing file")
-print("This is my updated line")
+"""
+Testing module for demonstrating print statements.
+"""
+
+
+def main() -> None:
+    """Main function to print test messages."""
+    print("This is the testing file")
+    print("This is my updated line")
+
+
+if __name__ == "__main__":
+    main()
